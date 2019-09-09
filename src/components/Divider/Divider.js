17fb49ce8466b5ec1d49f7987ad892efac6ec105
Nathan Fowler <luxurysandbox@gmail.com>
2019-09-08 14:53:29 -1000

    added styling props to divider, styling to testimonials, animations

--- src/components/Divider/Divider.js
+++ src/components/Divider/Divider.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Divider.css';
 
-const Divider = ({ width, className }) => {
-  return <div className={"Divider " + className} style={{width: width}} />
+const Divider = ({ width, className, border }) => {
+  return <div className={"Divider " + className} style={{width: width, border: border}} />
 }
 
 Divider.defaultProps = {
-  width: '75%'
+  width: '75%',
+  border: '1px solid #0a2c46'
 }
 export default Divider;
--- src/components/Testimonial/Testimonial.js
+++ src/components/Testimonial/Testimonial.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import './Testimonial.css';
 import Divider from '../Divider/Divider';
-import MdKeyboardArrowLeft from 'react-icons/lib/md/keyboard-arrow-left';
-import MdKeyboardArrowRight from 'react-icons/lib/md/keyboard-arrow-right';
+import MdKeyboardArrowDown from 'react-icons/lib/md/more-horiz';
 
 const Testimonial = ({id, logo, alt, quote, person, title, industry, plusOne, minusOne, faded}) => {
   if (faded) {
     setTimeout(() => {
       let divs = document.getElementsByClassName('fade')
       for (let x = 0; x < divs.length; x++) {
-        divs[x].classList.remove('fade-out')
+        divs[x].classList.remove( 'transform-x', 'fade-out')
       }
     }, 500);
   }
   function addFadeOutClass (cb, id) {
     let divs = document.getElementsByClassName('fade')
     for (let x = 0; x < divs.length; x++) {
-      divs[x].classList.add("fade-out")
+      divs[x].classList.add( "transform-x", 'fade-out' )
     }
     setTimeout(() => {
       cb(id)
@@ -24,18 +23,15 @@ const Testimonial = ({id, logo, alt, quote, person, title, industry, plusOne, mi
   }
 
   return (
-    <section className="Testimonial--item" key={id} >
-      <img className="Testimonial--img fade" src={logo} alt={alt + ' logo'} />
-      <h4 className="Testimonial--quote fade">"{quote}"</h4>
-      <Divider className="fade" width={'10%'}/>
-      <h5 className="Testimonial--person fade">{person} : {title}</h5>
-      <h6 className="Testimonial--industry fade">Industry: {industry}</h6>
-      <div className="Testimonial--button-wrapper">
-        <button className="Testimonial--button" role='navigation' onClick={() => addFadeOutClass(minusOne)}>
-          <MdKeyboardArrowLeft color="f7941d" size={20} />
-        </button>
-        <button className="Testimonial--button" role="navigation" onClick={() => addFadeOutClass(plusOne)}>
-          <MdKeyboardArrowRight color="f7941d" size={20} />
+    <section className="Testimonial" >
+      <div className="Testimonial--item" key={id} >
+        <img className="Testimonial--img fade transform" src={logo} alt={alt + ' logo'} />
+        <h4 className="Testimonial--quote fade transform">"{quote}"</h4>
+        <Divider border={'1px solid #f7941d'} className="fade transform" width={'10%'}/>
+        <h5 className="Testimonial--person fade transform">{person} : {title}</h5>
+        <h6 className="Testimonial--industry fade transform">Industry: {industry}</h6>
+        <button className="Testimonial--button" role='navigation' value="left" onClick={(e) => addFadeOutClass(minusOne, e.target.value)}>
+          <MdKeyboardArrowDown color="#f7941d" size={20} />
         </button>
       </div>
     </section>
